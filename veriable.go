c4391e165fe83250c1a3465acf15bbada913fdd8
pandapassion1812 <112780949+pandapassion1812@users.noreply.github.com>
2022-09-04 11:43:04 +0530

    system

---
+++ veriable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := 10000
+	fmt.Println(s)
+
+	s = 1000
+
+	fmt.Println(s)
+
+	y := 121
+	fmt.Println(y)
+	y = y + s
+	fmt.Println(y)
+
+	z := "This is ashish"
+	fmt.Println(z)
+
+}
